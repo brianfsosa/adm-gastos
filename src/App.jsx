cc70938742b593380b4ebaa61bf2a36189355875
Brian <brianfsosa@gmail.com>
2022-05-04 20:17:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Nav from './components/Nav'
+import Table from './components/Table'
+
+function App() {
+
+
+  return (
+    <div className="flex">
+      <Nav />
+      <Table />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["index.htm", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
