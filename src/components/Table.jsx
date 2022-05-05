19bcc17884a63b406bbfbabfa92f41c084d13195
Brian <brianfsosa@gmail.com>
2022-05-05 19:55:19 -0300

    table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,16 +7,16 @@ function Table() {
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" className="px-6 py-3">
-                    Product name
+                    FECHA
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Color
+                    DESCRIPCION
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Category
+                    MONTO
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Price
+                    CATEGORIA
                 </th>
                 <th scope="col" className="px-6 py-3">
                     <span className="sr-only">Edit</span>
